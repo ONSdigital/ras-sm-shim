d56e52b37ba0d9499a28d845d92298f6e98a56b1
Gareth Bult <gareth@bult.co.uk>
2017-06-26 16:23:49 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ras-cases-shim
+
+This module is used to inject routes into the API gatewaye
---
+++ controllers/respondents.py
@@ -0,0 +1,116 @@
+def stub1(self):
+    pass
+
+def stub2(self):
+    pass
+
+def stub3(self):
+    pass
+
+def stub4(self):
+    pass
+
+def stub5(self):
+    pass
+
+def stub6(self):
+    pass
+
+def stub7(self):
+    pass
+
+def stub8(self):
+    pass
+
+def stub9(self):
+    pass
+
+def stub10(self):
+    pass
+
+def stub11(self):
+    pass
+
+def stub12(self):
+    pass
+
+def stub13(self):
+    pass
+
+def stub14(self):
+    pass
+
+def stub15(self):
+    pass
+
+def stub16(self):
+    pass
+
+def stub17(self):
+    pass
+
+def stub18(self):
+    pass
+
+def stub19(self):
+    pass
+
+def stub20(self):
+    pass
+
+
+
+
+def getArchive(self):
+    pass
+
+def getArchives(self):
+    pass
+
+
+def saveDraft(self):
+    pass
+
+
+def deleteDraft(self):
+    pass
+
+
+def getDraft(self):
+    pass
+
+
+def updateDrafts(self):
+    pass
+
+
+def getDrafts(self):
+    pass
+
+
+def sendMessage(self):
+    pass
+
+
+def deleteMessage(self):
+    pass
+
+
+def getMessage(self):
+    pass
+
+
+def updateMessage(self):
+    pass
+
+
+def getMessages(self):
+    pass
+
+
+def getThread(self):
+    pass
+
+
+def getThreadsForRU(self):
+    pass
